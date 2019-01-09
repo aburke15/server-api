0796858fcc9390bfbb835e38380cb785c17f407c
Andre Burke <aburkex24@gmail.com>
2019-01-08 20:07:33 -0700

    added the GitHub repository dto

---
+++ AppData/Dtos/GitHubRepoDto.cs
@@ -0,0 +1,26 @@
+using System;
+using Newtonsoft.Json;
+
+namespace ServerApi.AppData.Dtos
+{
+    public class GitHubRepoDto
+    {
+        [JsonProperty(PropertyName = "created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "forks")]
+        public int Forks { get; set; }
+
+        [JsonProperty(PropertyName = "html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonProperty(PropertyName = "language")]
+        public string Language { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
