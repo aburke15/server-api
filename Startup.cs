e91900a1e40e1f6e7f7219e9354182566a7ac05f
Andre Burke <aburkex24@gmail.com>
2019-01-09 22:30:16 -0700

    made slight adjusts in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -16,12 +16,8 @@ namespace ServerApi
     {
         private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        //public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) 
+            => _configuration = configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
