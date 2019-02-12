18fc2666ca46c0c54d01c4e941739768e04d9a04
Andre Burke <aburkex24@gmail.com>
2019-02-11 20:15:05 -0700

    forced a deploy to production environment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -19,6 +20,7 @@ namespace ServerApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseEnvironment("Production") // find out how to change this to transform
                 .UseUrls("http://*:5300")
                 .UseStartup<Startup>();
     }
